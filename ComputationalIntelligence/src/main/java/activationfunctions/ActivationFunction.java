9ce55526a47e483f2964b031e4c897fb94574267
Christos Christidis <48795138+CChristidis@users.noreply.github.com>
2022-12-27 21:49:25 +0200

    Delete ComputationalIntelligence directory

--- ComputationalIntelligence/src/main/java/activationfunctions/ActivationFunction.java
+++
@@ -1,7 +0,0 @@
-package main.java.activationfunctions;
-
-public interface ActivationFunction {
-	public float activateOutput(float output);
-	public float derivative(float output);
-
-}
--- ComputationalIntelligence/src/main/java/activationfunctions/ActivationFunctionStrategy.java
+++
@@ -1,9 +0,0 @@
-package main.java.activationfunctions;
-
-public abstract class ActivationFunctionStrategy implements ActivationFunction {
-
-	public abstract float activateOutput(float output);
-	public abstract float derivative(float output);
-
-
-}
